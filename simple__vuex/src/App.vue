393349cedb017362754b3d7d279b5ad766453951
Maxim Koshman <maksimkoshman090@gmail.com>
2020-01-10 22:17:12 +0300

    add vuex app for making posts

---
+++ simple__vuex/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <PostForm/>
+    <h1>{{postCount}}</h1>
+    <div class="post" v-for="post in validPosts" :key="post.id">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex';
+import PostForm from './components/PostForm'
+export default {
+  name: 'app',
+  components: {PostForm},
+  computed: mapGetters(['validPosts', 'postCount']),
+  methods: mapActions(['fetchPosts']),
+  async mounted() {
+    // this.$store.dispatch('fetchPosts')
+    this.fetchPosts();
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 60px auto;
+  width: 400px;
+  text-align: center;
+  color: #333;
+}
+
+.post {
+  border: 1px solid #999;
+  border-radius: 2px;
+  margin: 10px 0;
+  padding: 5px;
+}
+
+</style>
