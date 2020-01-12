af45be15b07e2ad0aef71287513504d11705a8bf
Maxim Koshman <maksimkoshman090@gmail.com>
2020-01-12 12:09:00 +0300

    new Vue app - todolist with vue-router

---
+++ todolist/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsum obcaecati mollitia sapiente fugiat neque. Ex, iure dolorum dignissimos quia aliquam fugiat itaque accusamus eum dolor, vel esse libero vero!</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
